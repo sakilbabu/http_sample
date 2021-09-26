ee60036077c8354584d880a9569d95cbb1f6d106
Md Shakil Ahamed <md.shakilahamedxox72@gmail.com>
2021-09-26 13:30:31 -0400

    "http sample"

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +9,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final url = "https://animechan.vercel.app/api/random";
+  String? result;
+  Future<http.Response> getAnimeQuotes() async {
+    final response = await http.get(Uri.parse(url));
+
+    print(response.statusCode);
+    return response;
+  }
+
+  @override
+  void initState() {
+    getAnimeQuotes().then((value) {
+      result = value.body;
+      setState(() {});
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: result == null ? CircularProgressIndicator() : Text(result!),
+      ),
+    );
   }
 }
